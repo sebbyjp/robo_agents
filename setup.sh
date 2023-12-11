7fcc8b07012f5f02bf3aa3a49dc41720553c4a09
Sebastian Peralta <peraltas@seas.upenn.edu>
2023-12-11 10:29:33 +0000

    fix shared_mutex

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@
 #!/bin/bash
 set -e
 # TODO(speralta): Add this to Cmake using git fetch
+sudo apt-get update && sudo apt-get upgrade -y
 git clone  https://github.com/google/googletest.git third_party/gtest \
  && cd third_party/gtest \
  && mkdir -p build && cd build \
